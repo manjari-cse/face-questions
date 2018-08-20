e5d7118facc8839fbbb386fb692e607d475526d1
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-08-20 09:37:12 +0530

    Create gcd of n numbers.c

---
+++ training/tcs_ninja/gcd of n numbers.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+	int n,a[100],i,small,count=0,j,gcd=1;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	small=a[0];
+	for(i=0;i<n;i++)
+	{
+		if(a[i]<small)
+		small=a[i];
+	}
+
+	for(i=1;i<=small;i++)
+	{count=0;
+	    for(j=0;j<n;j++)
+	    {
+	        if(a[i]%i==0)
+	        {
+	            count++;
+	        }
+	    }
+	    if(count==n)
+	    {
+	        gcd=i;
+	    }
+	}
+		printf("%d",gcd);
+}
