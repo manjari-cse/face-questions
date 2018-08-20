3f424e918d37c7d86733f99e3d90554b08bacb0a
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-08-20 10:37:54 +0530

    Update time difference.c

--- training/tcs_ninja/time difference.c
+++ training/tcs_ninja/time difference.c
@@ -4,13 +4,11 @@
    /*i/p:08:58:3
          07:58:5
      o/p:00:59:58*/
-     #include<stdio.h>
+    #include<stdio.h>
 void main()
 {int hr1,min1,sec1,hr2,min2,sec2,hr3,min3,sec3;
     scanf("%d:%d:%d",&hr1,&min1,&sec1);
     scanf("%d:%d:%d",&hr2,&min2,&sec2);
-   // if(hr1>hr2)
-    //{
     if(sec1<sec2)
     {
         sec1=sec1+60;
@@ -35,8 +33,8 @@ void main()
     {
         min3=min1-min2;
     }
-    if(hr1==0)
-    hr3=hr2-hr1;
+    if(hr1>hr2)
+    hr3=hr1-hr2;
     else
     hr3=hr2-hr1;
 printf("%d:%d:%d",hr3,min3,sec3);
