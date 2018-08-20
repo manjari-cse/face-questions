8d1cd0ed5a5aa0c34b1bc387a9b8d332a63753d7
manjari-cse <30829920+manjari-cse@users.noreply.github.com>
2018-08-20 15:32:50 +0530

    Update roman to decimal.c

--- training/tcs_ninja/roman to decimal.c
+++ training/tcs_ninja/roman to decimal.c
@@ -37,3 +37,50 @@ printf("%d",dec);
 
     return 0;
 }
+//alternate way
+
+#include <stdio.h>
+#include<string.h>
+int convert(char a)
+{
+    switch(a)
+    {
+        case 'I':
+        return 1;
+        case 'V':
+        return 5;
+        case 'X':
+        return 10;
+        case 'L':
+        return 50;
+    
+    }
+}
+
+int main()
+{
+char s[100];
+int len,i,a[100],dec=0;
+scanf("%s",s);
+len=strlen(s);
+for(i=0;i<len;i++)
+{
+a[i]=convert(s[i]);
+}
+
+dec=a[len-1];
+for(i=len-1;i>0;i--)
+{
+  if(a[i-1]>=a[i])
+  {
+      dec=dec+a[i-1];
+  }
+ 
+  else
+  {
+      dec=dec-a[i-1];
+  }
+}
+printf("%d",dec);
+    return 0;
+}
